3c570f471f51bc6e845cb15693ae8fe2cfc9276c
DarKTeaMoNe <alimomeni200900@gmail.com>
2016-10-09 09:54:03 +0330

    Update spammer.lua

--- plugins/spammer.lua
+++ plugins/spammer.lua
@@ -4,7 +4,7 @@ function run(msg, matches)
 
   local tex = matches[1]
   local sps = matches[2]
-  local sp = '\nBy @SpartaSoft'
+  local sp = '\nBy @DarKTeaMoNe'
   for i=1, tex, 1 do 
       sp = '\n'..sps..'\n'..sp
   end 
